dc9d98adfa2b749819cebb89c90af1f3f35b94ca
Alice Ewaldsen <alice.ewaldsen@hotmail.com>
2026-06-10 21:21:10 +0200

    Updated detail page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,9 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/icon-dark-32x32.png",
         media: "(prefers-color-scheme: light)",
+        // :/
       },
       {
         url: "/icon-dark-32x32.png",
